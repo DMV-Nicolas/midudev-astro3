bc456746b78865e17bd89dff3ca52323ebec7edd
DMV-Nicolas <dmvnicolas@gmail.com>
2025-08-29 18:38:30 -0500

    release and 404 page have been added

---
+++ src/services/spacex.ts
@@ -0,0 +1,37 @@
+import { type APISpaceXResponse, type Doc } from "../types/api"
+
+interface GetLatestLaunchesOptions {
+  limit: number
+}
+
+export async function getLatestLaunches({ limit }: GetLatestLaunchesOptions) {
+  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        limit: limit,
+        sort: {
+          date_unix: "asc",
+        },
+      },
+    }),
+  })
+
+  return (await res.json()) as APISpaceXResponse
+}
+
+interface GetLaunchOptions {
+  id: string
+}
+
+export async function getLaunch({ id }: GetLaunchOptions) {
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`)
+
+  const data = (await res.json()) as Doc
+
+  return data
+}
